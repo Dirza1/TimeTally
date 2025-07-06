d136991d85a89453184f4cf3bc6dde8e5b9fe5ab
Dirza1 <jasper.olthof@xs4all.nl>
2025-07-06 11:19:29 +0200

    implemented first reset command

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ Copyright © 2025 Jasper Olthof-Donker <jasper.olthof@xs4all.nl>
 */
 package main
 
-import "github.com/Dirza1/Time-and-expence-registration/cmd"
+import (
+	"github.com/Dirza1/Time-and-expence-registration/cmd"
+)
 
 func main() {
 	cmd.Execute()
