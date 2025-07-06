2083b28be638011686b5b083172550f3983ec748
Dirza1 <jasper.olthof@xs4all.nl>
2025-07-06 23:10:20 +0200

    completed reset command

---
+++ cmd/reset.go
@@ -0,0 +1,88 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Dirza1/Time-and-expence-registration/internal/utils"
+	"github.com/joho/godotenv"
+	"github.com/spf13/cobra"
+)
+
+// resetCmd represents the reset command
+var Type string
+var Confirm bool
+var Password string
+
+var resetCmd = &cobra.Command{
+	Use:   "reset",
+	Short: "Removes all data from the finance and or timeregistration ddatabase",
+	Long: `This command removes all data from the finance and or timeregistration database.
+	NOTE: This is a permenant action. The data will be lost and if there are no backups the data will be lost!
+	Execution of this command requires an additional password to protect against axidental use.
+	Additionaly this comman uses 3 required flags for the database to be deleted, confirming to delete and the password.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if Confirm != true {
+			log.Fatal("Confirm flag not set correctly.")
+		}
+		godotenv.Load(".env")
+		setPasword := os.Getenv("reset_password")
+		if setPasword != Password {
+			log.Fatal("Incorrect password supplied")
+		}
+		queries := utils.DatabaseConnection()
+		switch Type {
+		case "Finance":
+			err := queries.ResetTransaction(context.Background())
+			if err != nil {
+				log.Fatal("error during reset: ", err)
+			}
+			fmt.Println("reset called on financee")
+		case "Time":
+			err := queries.ResetTimeRegistration(context.Background())
+			if err != nil {
+				log.Fatal("error during reset: ", err)
+			}
+			fmt.Println("reset called on time")
+		case "All":
+			err := queries.ResetTimeRegistration(context.Background())
+			if err != nil {
+				log.Fatal("error during reset: ", err)
+			}
+			err = queries.ResetTransaction(context.Background())
+			if err != nil {
+				log.Fatal("error during reset: ", err)
+			}
+			fmt.Println("reset called on all")
+		default:
+			fmt.Println("Incorrect use of Type flag. Use either Finance, Time or All. Ensure correct capitalisation")
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(resetCmd)
+
+	resetCmd.Flags().StringVarP(&Type, "type", "t", "all", "Used to diferenciate which database needs to be deleted. Use Finance, Time or all as input. (required)")
+	resetCmd.MarkFlagRequired("type")
+
+	resetCmd.Flags().BoolVarP(&Confirm, "confirm", "c", false, "Used to confirm the database delition. Type true after the flag (required)")
+	resetCmd.MarkFlagRequired("confirm")
+
+	resetCmd.Flags().StringVarP(&Password, "password", "p", "", "Additional password required for delition. (required)")
+	resetCmd.MarkFlagRequired("password")
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// resetCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// resetCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
