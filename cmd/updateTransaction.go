035ea941d2c1915c6b635096f7c22fe270dd4ac9
Dirza1 <jasper.olthof@xs4all.nl>
2025-07-09 14:45:24 +0200

    added update and delete command

--- cmd/updateTransaction.go
+++ cmd/updateTransaction.go
@@ -25,12 +25,9 @@ var updateTransactionID string
 var updateTransactionCmd = &cobra.Command{
 	Use:   "updateTransaction",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `This command will allow you to update a transaction.
+	It will require the date of the activity, theamount in cents and what it was spent on.
+	Later this entry is modifiable and deletable.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		layout := "02-01-2006"
 		query := utils.DatabaseConnection()
